8a57de78d6b7a5ecb29043534b2807e058c517f9
Joshua Ike <79171733+kxng0109@users.noreply.github.com>
2026-08-17 15:35:13 +0100

    feat: initial project setup with Maven wrapper and Spring Boot starter

---
+++ src/test/java/io/github/kxng0109/aegisgate/AegisGateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kxng0109.aegisgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
